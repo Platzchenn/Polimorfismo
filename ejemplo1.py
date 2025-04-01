e3a0e53e8fc592d944d33d59a539a7aa5c6511b9
Platzchenn <1034776102@teinco.edu.co>
2025-04-01 09:02:01 -0500

    Create ejemplo1.py

---
+++ ejemplo1.py
@@ -0,0 +1,29 @@
+#Creación de la superclase "Animal"
+class Animal:
+  #Definir el método "sonido"
+  def sonido(self):
+    pass
+
+#Creación de la subclase 1, junto con su método y atributo
+class Perro(Animal):
+  #Se asigna ell mismo método de la superclase "Animal"
+  def sonido (self):
+    return "Guau"
+
+#Creación de la subclase 2, junto con su método y atributo
+class Gato(Animal):
+  #Se asigna el mismo método de la superclase "Animal"
+  def sonido (self):
+    return "Miau"
+
+@Creación de la función "Hacer sonido"
+hacer_sonido(animal:Animal):
+  print(animal.sonido())
+
+#Creación de los objetos 
+perro=Perro()
+gato=Gato()
+
+#Salida de los objetos 
+hacer_sonido(perro)
+hacer_sonido(gato)
